24c20b4cb0384997ffc886a54cd996ac0bb1404b
Guilherme Ponciano <gponciano00@gmail.com>
2024-11-13 19:49:08 -0300

    Update api.js

--- src/servises/api.js
+++ src/servises/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://api.render.com/deploy/srv-csqia23tq21c73dl09f0?key=lOhFcn1_FAw'
+    baseURL: 'https://api.render.com/deploy/srv-csqio8jgbbvc738ml090?key=RuRbwHmbd7w'
 })
 export default api
