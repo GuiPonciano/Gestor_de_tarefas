c7fbfef650232b40ed28d9705793d246c6907965
Guilherme <gponciano00@gmail.com>
2024-11-13 17:10:36 -0300

    Ajuste na URL do Back-End

--- src/servises/api.js
+++ src/servises/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://api.render.com/deploy/srv-csqfth08fa8c73c4mtqg?key=eK4tByrLKx4'
 })
 export default api
