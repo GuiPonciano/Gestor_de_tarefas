7f549f8d8ebf4650f3a6d515cb3f5ada8af319c9
Guilherme <gponciano00@gmail.com>
2024-11-14 10:55:28 -0300

    Ajustes da URL(Back-End

--- src/servises/api.js
+++ src/servises/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://api.render.com/deploy/srv-csqia23tq21c73dl09f0?key=lOhFcn1_FAw'
+    baseURL: 'https://gestor-de-tarefas-back-end.onrender.com/lista'
 })
 export default api
